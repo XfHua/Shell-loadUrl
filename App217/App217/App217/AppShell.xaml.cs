87e9055f5828953ec158701ad97b0e79e29c182f
XfHua <jasonHuaxf@163.com>
2020-04-23 10:37:37 +0800

    Add files via upload

---
+++ App217/App217/App217/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App217
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ App217/App217/App217/Views/BrowserPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App217.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BrowserPage : ContentPage
+    {
+        public BrowserPage()
+        {
+            InitializeComponent();
+
+            Launcher.OpenAsync("https://www.xamarin.com");
+        }
+    }
+}
